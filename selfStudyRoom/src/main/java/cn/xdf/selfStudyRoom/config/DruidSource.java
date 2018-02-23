c702d1963d91593585a33109878683a196cd6821
liuwei02 <975211164@qq.com>
2018-02-23 14:19:26 +0800

    新增数据源监控

--- selfStudyRoom/src/main/java/cn/xdf/selfStudyRoom/config/DruidSource.java
+++ selfStudyRoom/src/main/java/cn/xdf/selfStudyRoom/config/DruidSource.java
@@ -3,12 +3,16 @@ package cn.xdf.selfStudyRoom.config;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 @PropertySource("classpath:jdbc.properties")
@@ -16,7 +20,7 @@ public class DruidSource {
 	
 	  @Value("${jdbc.url}")
 	  private String dbUrl;
-	 
+	  	 
 	  @Value("${jdbc.username}")
 	  private String username;
 	 
@@ -67,6 +71,15 @@ public class DruidSource {
 	 
 	  @Value("${jdbc.connectionProperties}")
 	  private String connectionProperties;
+	  
+	  @Value("${jdbc.useGlobalDataSourceStat}")  
+	  private boolean useGlobalDataSourceStat;  
+	      
+	  @Value("${jdbc.druidLoginName}")  
+	  private String druidLoginName;  
+	      
+	  @Value("${jdbc.druidPassword}")  
+	  private String druidPassword;  
 	 
 	  public String getDbUrl() {
 	    return dbUrl;
@@ -212,7 +225,7 @@ public class DruidSource {
 	    this.connectionProperties = connectionProperties;
 	  }
 	 
-	  @Bean   //声明其为Bean实例
+	  @Bean(name="dataSource",destroyMethod = "close", initMethod="init") //声明其为Bean实例
 	  @Primary //在同样的DataSource中，首先使用被标注的DataSource
 	  public DataSource dataSource() throws SQLException {
 	    DruidDataSource datasource = new DruidDataSource();
@@ -222,7 +235,7 @@ public class DruidSource {
 	    datasource.setPassword(password);
 	    datasource.setDriverClassName(driverClassName);
 	 
-	    //configuration
+
 	    datasource.setInitialSize(initialSize);
 	    datasource.setMinIdle(minIdle);
 	    datasource.setMaxActive(maxActive);
@@ -234,12 +247,38 @@ public class DruidSource {
 	    datasource.setTestOnBorrow(testOnBorrow);
 	    datasource.setTestOnReturn(testOnReturn);
 	    datasource.setPoolPreparedStatements(poolPreparedStatements);
-	    datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
-	 
+	    datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);	 
 	    datasource.setFilters(filters);
-	 
+	    datasource.setConnectionProperties(connectionProperties);  
+	    datasource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);
 	 
 	    return datasource;
 	  }
 
+   /////////////////////////////////下面是druid监控访问的设置 /////////////////////////////////////////////////////////////
+	@Bean
+	public ServletRegistrationBean druidServlet() {
+		ServletRegistrationBean reg = new ServletRegistrationBean();
+		reg.setServlet(new StatViewServlet());
+		reg.addUrlMappings("/druid/*"); //url匹配
+		reg.addInitParameter("allow", "10.167.3.50,127.0.0.1"); // IP白名单(没有配置或者为空，则允许所有访问)
+		reg.addInitParameter("deny", "192.168.16.111"); // IP黑名单(存在共同时，deny优先于allow)
+		reg.addInitParameter("loginUsername", this.druidLoginName);// 登录名
+		reg.addInitParameter("loginPassword", this.druidPassword);// 登录密码
+		reg.addInitParameter("resetEnable", "false"); // 禁用HTML页面上的“Reset All”功能
+		return reg;
+	}
+
+	@Bean(name = "druidWebStatFilter")
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new WebStatFilter());
+		filterRegistrationBean.addUrlPatterns("/*");
+		filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*"); // 忽略资源
+		filterRegistrationBean.addInitParameter("profileEnable", "true");
+		filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+		filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+		return filterRegistrationBean;
+	}
+	  
 }
--- selfStudyRoom/src/main/java/cn/xdf/selfStudyRoom/config/WebSecurityConfig.java
+++ selfStudyRoom/src/main/java/cn/xdf/selfStudyRoom/config/WebSecurityConfig.java
@@ -90,6 +90,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/common/**");
         web.ignoring().antMatchers("/loginCheck**");
+        web.ignoring().antMatchers("/druid/*");
     }
     
     
